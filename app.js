56baf5826f0a7dadb15b645ab808792701e5014a
Vishwajeet KUmbhar <vishwajeetkumbhar97@gmail.com>
2023-12-06 23:14:06 +0530

    server change

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const mongoose = require("mongoose")
 require("./db/conn");
 const users = require("./model/userSchema")
 const cors = require("cors")
-const port = process.env.PORT || 8006
+const PORT = process.env.PORT || 8006
 const router = require("./routes/route")
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log("server is start on Port 8006")
 })
 
